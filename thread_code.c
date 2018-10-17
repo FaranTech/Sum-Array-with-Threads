344f54122861c90322c0ce6a1fb6cb87e5c1b69f
FaranTech <abubakarnazir10@gmail.com>
2018-10-17 23:47:58 +0500

    Update thread_code.c

--- thread_code.c
+++ thread_code.c
@@ -20,26 +20,35 @@ int main()
 {
 	pthread_t t1,t2,t3,t4,t5,t6,t7,t8,t9,t10;
 	int sum_t1,sum_t2,sum_t3,sum_t4,sum_t5,sum_t6,sum_t7,sum_t8,sum_t9,sum_t10;
+	
 	pthread_create(&t1,NULL,Add,(void*)(0*chunk_size),(void*)(1*chunk_size));
-	pthread_create(&t2,NULL,Add,(void*)(1*chunk_size),(void*)(2*chunk_size));
-	pthread_create(&t3,NULL,Add,(void*)(2*chunk_size),(void*)(3*chunk_size));
-	pthread_create(&t4,NULL,Add,(void*)(3*chunk_size),(void*)(4*chunk_size));
-	pthread_create(&t5,NULL,Add,(void*)(4*chunk_size),(void*)(5*chunk_size));
-	pthread_create(&t6,NULL,Add,(void*)(5*chunk_size),(void*)(6*chunk_size));
-	pthread_create(&t7,NULL,Add,(void*)(6*chunk_size),(void*)(7*chunk_size));
-	pthread_create(&t8,NULL,Add,(void*)(7*chunk_size),(void*)(8*chunk_size));
-	pthread_create(&t9,NULL,Add,(void*)(8*chunk_size),(void*)(9*chunk_size));
-	pthread_create(&t10,NULL,Add,(void*)(9*chunk_size),(void*)(10*chunk_size));
-
 	pthread_join(&t1,(void**)&sum_t1);
+	
+	pthread_create(&t2,NULL,Add,(void*)(1*chunk_size),(void*)(2*chunk_size));
 	pthread_join(&t2,(void**)&sum_t2);
-	pthread_join(&t3,(void**)&sum_t3);
+	
+	pthread_create(&t3,NULL,Add,(void*)(2*chunk_size),(void*)(3*chunk_size));
+	pthread_join(&t3,(void**)&sum_t3);	
+	
+	pthread_create(&t4,NULL,Add,(void*)(3*chunk_size),(void*)(4*chunk_size));
 	pthread_join(&t4,(void**)&sum_t4);
+	
+	pthread_create(&t5,NULL,Add,(void*)(4*chunk_size),(void*)(5*chunk_size));
 	pthread_join(&t5,(void**)&sum_t5);
+	
+	pthread_create(&t6,NULL,Add,(void*)(5*chunk_size),(void*)(6*chunk_size));
 	pthread_join(&t6,(void**)&sum_t6);
+	
+	pthread_create(&t7,NULL,Add,(void*)(6*chunk_size),(void*)(7*chunk_size));
 	pthread_join(&t7,(void**)&sum_t7);
+	
+	pthread_create(&t8,NULL,Add,(void*)(7*chunk_size),(void*)(8*chunk_size));
 	pthread_join(&t8,(void**)&sum_t8);
+	
+	pthread_create(&t9,NULL,Add,(void*)(8*chunk_size),(void*)(9*chunk_size));
 	pthread_join(&t9,(void**)&sum_t9);
+	
+	pthread_create(&t10,NULL,Add,(void*)(9*chunk_size),(void*)(10*chunk_size));
 	pthread_join(&t10,(void**)&sum_t10);
 
 int Final_sum=0;
